62f8954358e0aec7359f8da8deb00b909b7dd8f9
Matt Stein <mattman2864@gmail.com>
2023-10-17 20:58:24 -0400

    basic movements of all pieces added

--- chess.py
+++ chess.py
@@ -7,10 +7,10 @@ SQUARE_SIZE = RESOLUTION[0]/8
 pieces = np.array([ 
          4,  2,  3,  6,  5,  3,  2,  4,
          1,  1,  1,  1,  1,  1,  1,  1,
-         0,  0,  0,  2,  0,  0,  0,  0,
-         0,  0,  0,  0,  0,  0,  0,  3,
-         0,  0,  0,  3,  0,  0,  0,  0,
-         3,  0,  0,  0,  0,  0,  0,  0,
+         0,  0,  0,  0,  0,  0,  0,  0,
+         0,  0,  0,  0,  5,  0,  0,  4,
+         0,  0,  0,  0,  -6,  0,  0,  0,
+         0,  0,  0,  0,  0,  0,  0,  0,
         -1, -1, -1, -1, -1, -1, -1, -1,
         -4, -2, -3, -6, -5, -3, -2, -4,
 ])
@@ -67,19 +67,29 @@ def findMoves(pieces, square):
         return []
     moves = []
     if abs(pieces[square]) == 1:
-        moves = findPawnMoves(pieces, square, pieces[square]>0)
+        moves = findPawnMoves(pieces, square, pieces[square])
     if abs(pieces[square]) == 2:
         moves = findKnightMoves(pieces, square)
     if abs(pieces[square]) == 3:
         moves = findBishopMoves(pieces, square)
+    if abs(pieces[square]) == 4:
+        moves = findRookMoves(pieces, square)
+    if abs(pieces[square]) == 5:
+        moves = np.union1d(findRookMoves(pieces, square), findBishopMoves(pieces, square))
+    if abs(pieces[square]) == 6:
+        moves = findKingMoves(pieces, square)
     return moves
     
 def findPawnMoves(pieces, square, color):
     moves = []
-    if not pieces[square+(1 if color else -1)*8]:
-        moves.append(square+(1 if color else -1)*8)
-    if (not pieces[square+(1 if color else -1)*16]) and square//8 in [1, 6]:
-        moves.append(square+(1 if color else -1)*16)
+    if not pieces[square+(1 if color>0 else -1)*8]:
+        moves.append(square+(1 if color>0 else -1)*8)
+        if (not pieces[square+(1 if color>0 else -1)*16]) and square//8 in [1, 6]:
+            moves.append(square+(1 if color>0 else -1)*16)
+    if pieces[square+(1 if color>0 else -1)*7] * color < 0:
+        moves.append(square+(1 if color>0 else -1)*7)
+    if pieces[square+(1 if color>0 else -1)*9] * color < 0:
+        moves.append(square+(1 if color>0 else -1)*9)
     return moves
 def findKnightMoves(pieces, square):
     moves = []
@@ -95,10 +105,60 @@ def findKnightMoves(pieces, square):
     return moves
 def findBishopMoves(pieces, square):
     moves = []
-    steps = [-1, 1]
-    for i in range(64):
-        if (i%9 == square%9 or i%7 == square%7) and squareColor(square) == squareColor(i):
-            moves.append(i)
+    steps = [(1, 1),(1, -1),(-1, 1),(-1, -1)]
+    for dx, dy in steps:
+        s = 1
+        while True:
+            try_move = ctn((ntc(square)[1]+dx*s, ntc(square)[0]+dy*s))
+            if not (squareColor(square) == squareColor(try_move)):
+                break
+            elif not isOnBoard(try_move):
+                break
+            elif pieces[try_move]:
+                if pieces[try_move] * pieces[square] < 0:
+                    moves.append(try_move)
+                    break
+                else:
+                    break
+            else:
+                moves.append(try_move)
+            s+=1
+        try_move = ctn((ntc(square)[0]+dx*s, ntc(square)[1]+dy*s))
+    return moves
+def findRookMoves(pieces, square):
+    moves = []
+    steps = [(0, 1),(1, 0),(-1, 0),(0, -1)]
+    for dx, dy in steps:
+        s = 1
+        while True:
+            try_move = ctn((ntc(square)[1]+dx*s, ntc(square)[0]+dy*s))
+            if try_move%8-square%8 != dx*s or try_move//8-square//8 != dy*s:
+                break
+            if not isOnBoard(try_move):
+                break
+            elif pieces[try_move]:
+                if pieces[try_move] * pieces[square] < 0:
+                    moves.append(try_move)
+                    break
+                else:
+                    break
+            else:
+                moves.append(try_move)
+            s+=1
+        try_move = ctn((ntc(square)[0]+dx*s, ntc(square)[1]+dy*s))
+    return moves
+def findKingMoves(pieces, square):
+    moves = []
+    for i in range(-8, 16, 8):
+        for j in range(-1, 2):
+            try_move = square + i + j
+            if i==j==0:
+                continue
+            if not isOnBoard(try_move):
+                continue
+            if abs(square%8-try_move%8) > 1 or pieces[try_move] * pieces[square] > 0:
+                continue
+            moves.append(try_move)
     return moves
 def ntc(num): # Square Number to Coordinates
     coord = (num//8, num%8)
@@ -106,12 +166,12 @@ def ntc(num): # Square Number to Coordinates
 def ctn(coords): # Coordinates to Square Number
     num = coords[1]*8 + coords[0]
     return num
-def isOnBoard(coords):
-    return 0<=ctn(coords)<=63
+def isOnBoard(square):
+    return 0<=square<=63
 def isOnEdge(square):
     return square%8 in [0, 7] or square//8 in [0,7]
 def squareColor(square):
-    return (ntc(square)[0] + ntc(square)[1]) % 2 == 0
+    return 'white' if (ntc(square)[0] + ntc(square)[1]) % 2 == 0 else 'black'
 
 pygame.init()
 screen = pygame.display.set_mode(RESOLUTION)
@@ -124,7 +184,6 @@ while True:
             pygame.quit
             exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
-            print('select')
             mpos = pygame.mouse.get_pos()
             new_square = int(mpos[1]//SQUARE_SIZE*8 + mpos[0]//SQUARE_SIZE)
             if new_square == selected_square:
